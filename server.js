431a52d6f80bdf0c5cccfcf2c1e00ab27610e084
Christian Nielsen <chrnielsen2003@gmail.com>
2023-12-07 13:12:47 +0100

    server

---
+++ server.js
@@ -0,0 +1,50 @@
+const express = require("express");
+const cors = require("cors");
+const app = express();
+const admin = require("firebase-admin");
+const serviceAccount = require("./arbi-463c1-firebase-adminsdk-8sdt2-ecfd650b5b.json");
+const bodyParser = require("body-parser");
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+});
+
+// Enable CORS
+app.use(
+  cors({
+    origin: "*",
+  })
+);
+
+app.use(bodyParser.json());
+
+// Define the root route
+app.post("/api/start", (req, res) => {
+  const data = req.body;
+
+  const db = admin.firestore();
+  const docRef = db
+    .collection("users")
+    .doc(data.dato + "-" + data.email)
+    .set(data);
+
+  docRef
+    .then(() => {
+      res.json({
+        message: "Hello, World!",
+        data: data,
+        status: "success",
+      });
+    })
+    .catch((error) => {
+      res.json({
+        message: "Hello, World!",
+        data: data,
+        status: "failed",
+      });
+    });
+});
+
+app.listen(1234, () => {
+  console.log("Server running on port 3000");
+});
